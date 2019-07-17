7f36ca7bd3cf5a38780d239f018e10f87830232d
KevTLW <kevinmcruzc@gmail.com>
2019-07-16 23:40:28 -0400

    Added CMS project

--- src/resources/personal/projects.js
+++ src/resources/personal/projects.js
@@ -1,11 +1,19 @@
 const projects = [
   {
     name: 'portfolio',
-    desc: 'My first professional web development portfolio in which I showcase projects I\'m currently working on along with others I\'ve worked on in the past',
+    desc: 'My first professional web development portfolio in which I showcase projects I\'m currently working on along with others I\'ve worked on in the past.',
     demo: require('../images/portfolio.gif'),
     site: 'https://kevtlw.github.io/',
     source: 'https://github.com/KevTLW/kevtlw.github.io/',
     stack: ['React', 'Sass']
+  },
+  {
+    name: 'RPI CMS 3.0',
+    desc: 'CMS is the system utilized at Rensselaer Polytechnic Institute to organize student clubs, add students to clubs, create club mailing lists, and add new officers.',
+    demo: require('../images/cms.png'),
+    site: 'https://cms.union.rpi.edu/',
+    source: null,
+    stack: ['React', 'Django', 'PostgreSQL', 'jQuery', 'Bootstrap']
   }
 ];
 
