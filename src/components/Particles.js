d3cf4ec76c7ccf477a322217c760ddc879d40067
KevTLW <kevinmcruzc@gmail.com>
2019-08-09 22:00:39 -0400

    Code cleanup and updated resume

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Particles = () => (
   <div id='particles'>
-    {[...Array(100)].map(i => <span className='particle' key={i}></span>)}
+    {[...Array(100)].map((_, i) => (
+      <span className='particle' key={i}></span>)
+    )}
   </div>
 );
 
